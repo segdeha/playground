3c521ea4b623638338ca1c1f2546a02f624c9ad2
Andrew Hedges <andrew@hedges.name>
2018-08-29 14:00:24 -0700

    Set up super basic routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+import Blocks from './components/Blocks';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <main>
-          <h1>Animation playground</h1>
-          <ul>
-              <li></li>
-          </ul>
-      </main>
+      <Fragment>
+        <header>
+            <h1>Animation playground</h1>
+        </header>
+        <main>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/blocks" component={Blocks} />
+                    <Route>
+                        <ul>
+                            <li>
+                                <Link to="/blocks">
+                                    Blocks
+                                </Link>
+                            </li>
+                        </ul>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </main>
+      </Fragment>
     );
   }
 }
