337084187f166b79575d46044ccbd3fa5d81ea89
Andrew Hedges <andrew@hedges.name>
2018-08-29 19:45:38 -0700

    Might have gone a little overboard with the animations

--- src/components/Block.js
+++ src/components/Block.js
@@ -5,6 +5,7 @@ import anime from 'animejs';
 import { randomColor } from '../lib/random';
 
 class Block extends Component {
+    directions = ['alternate', 'reverse'];
     easings = ['easeInQuad', 'easeOutQuad', 'easeInOutQuad', 'easeInCubic', 'easeOutCubic', 'easeInOutCubic', 'easeInQuart', 'easeOutQuart', 'easeInOutQuart', 'easeInQuint', 'easeOutQuint', 'easeInOutQuint', 'easeInSine', 'easeOutSine', 'easeInOutSine', 'easeInExpo', 'easeOutExpo', 'easeInOutExpo', 'easeInCirc', 'easeOutCirc', 'easeInOutCirc', 'easeInBack', 'easeOutBack', 'easeInOutBack', 'easeInElastic', 'easeOutElastic', 'easeInOutElastic'];
 
     constructor(props) {
@@ -12,22 +13,42 @@ class Block extends Component {
 
         let background = randomColor();
         let delay = anime.random(0, 250);
-        let direction = 'alternate';
+        let direction = this.directions[anime.random(0, this.directions.length - 1)];
         let duration = anime.random(500, 2000);
         let easing = this.easings[anime.random(0, this.easings.length - 1)];
+        let elasticity = anime.random(0, 1000);
         let loop = true;
-        let rotate = `${anime.random(1,2)}turn`;
-        let scale = anime.random(0.75, 1.25);
-        let translateX = [`${anime.random(-50, 50)}px`, `${anime.random(-50, 50)}px`];
+        let rotate = `${anime.random(90, 450)}deg`;
+        let scaleRange = { min: 50, max: 150 };
+        let scaleFactor = 100;
+        let scaleX = [];
+        let scaleY = [];
+        let translateX = [];
+        let translateY = [];
+        for (let i = 0; i < 10; i += 1) {
+            scaleX.push(this._keyframe(scaleRange, scaleFactor));
+            scaleY.push(this._keyframe(scaleRange, scaleFactor));
+            translateX.push(this._keyframe());
+            translateY.push(this._keyframe());
+        }
 
-        this.state = { background, delay, direction, duration, easing, loop, rotate, scale, translateX };
+        this.state = { background, delay, direction, duration, easing, elasticity, loop, rotate, scaleX, scaleY, translateX, translateY };
 
         this._animate = this._animate.bind(this);
     }
 
+    _keyframe(range = { min: -50, max: 50 }, factor = 1) {
+        let delay = anime.random(0, 250);
+        let duration = anime.random(500, 2000);
+        let easing = this.easings[anime.random(0, this.easings.length - 1)];
+        let elasticity = anime.random(0, 1000);
+        let value = anime.random(range.min, range.max) / factor;
+        return { delay, duration, easing, elasticity, value };
+    }
+
     _animate() {
-        let { delay, direction, duration, easing, loop, rotate, scale, translateX } = this.state;
-        anime({ delay, direction, duration, easing, loop, rotate, scale, targets: this.targets, translateX });
+        let { delay, direction, duration, easing, elasticity, loop, rotate, scaleX, scaleY, translateX, translateY } = this.state;
+        anime({ targets: this.targets, delay, direction, duration, easing, elasticity, loop, rotate, scaleX, scaleY, translateX, translateY });
     }
 
     componentDidMount() {
@@ -40,6 +61,7 @@ class Block extends Component {
         // define Item here so each block gets a differnt color
         const Item = styled.div`
             background: ${background};
+            box-shadow: rgba(0, 0, 0, 0.25) 0 4px 8px;
             height: 0;
             padding-bottom: 100%;
             width: 100%;
